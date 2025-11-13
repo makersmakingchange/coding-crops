95eef34515a3815e2d2ddfe5631d0026efe28734
MMC_Jorene <jorenen@neilsquire.ca>
2025-11-13 00:10:28 -0800

    Add math categories and loops/logic blocks

--- src/blockly/toolboxes/ToolboxLevel2.ts
+++ src/blockly/toolboxes/ToolboxLevel2.ts
@@ -21,7 +21,46 @@ export const ToolboxLevel2 = {
                              }
                          }
                      }
-                }
+                },
+                {
+                    type: 'controls_if',
+                    kind: 'block',
+                    id: 'if_block',
+                },
+                {
+                    type: 'logic_compare',
+                    kind: 'block',
+                    fields: {
+                        OP: 'EQ',
+                    },
+                },
+                {
+                    type: 'logic_operation',
+                    kind: 'block',
+                    fields: {
+                        OP: 'AND',
+                    },
+                },
+                {
+                    type: 'logic_negate',
+                    kind: 'block',
+                },
+                {
+                    type: 'logic_boolean',
+                    kind: 'block',
+                    fields: {
+                        BOOL: 'TRUE',
+                    },
+                },
+                {
+                    type: 'logic_null',
+                    kind: 'block',
+                    enabled: false,
+                },
+                {
+                    type: 'logic_ternary',
+                    kind: 'block',
+                },
             ]
         },
         {
--- src/blockly/toolboxes/ToolboxLevel3.ts
+++ src/blockly/toolboxes/ToolboxLevel3.ts
@@ -7,7 +7,7 @@ export const ToolboxLevel3 = {
     contents: [
         {
             kind: 'category',
-            name: 'Logic Center (TBD)',
+            name: 'Logic Center',
             categorystyle: 'loop_category',
             contents: [
                 {
@@ -54,8 +54,175 @@ export const ToolboxLevel3 = {
                 {
                     kind: 'block',
                     type: 'math_number',
-                }
+                },
+                {
+                    type: 'controls_whileUntil',
+                    kind: 'block',
+                    fields: {
+                        MODE: 'WHILE',
+                    },
+                },
+                {
+                    type: 'controls_for',
+                    kind: 'block',
+                    fields: {
+                        VAR: {
+                            name: 'i',
+                        },
+                    },
+                    inputs: {
+                        FROM: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 1,
+                                },
+                            },
+                        },
+                        TO: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 10,
+                                },
+                            },
+                        },
+                        BY: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 1,
+                                },
+                            },
+                        },
+                    },
+                },
+                {
+                    type: 'controls_flow_statements',
+                    kind: 'block',
+                    enabled: true,
+                    fields: {
+                        FLOW: 'BREAK',
+                    },
+                },
             ]
+        },
+        {
+            kind: 'category',
+            name: 'Math',
+            categorystyle: 'math_category',
+            contents: [
+                {
+                    type: 'math_number',
+                    kind: 'block',
+                    fields: {
+                        NUM: 123,
+                    },
+                },
+                {
+                    type: 'math_arithmetic',
+                    kind: 'block',
+                    fields: {
+                        OP: 'ADD',
+                    },
+                    inputs: {
+                        A: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 1,
+                                },
+                            },
+                        },
+                        B: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 1,
+                                },
+                            },
+                        },
+                    },
+                },
+                {
+                    type: 'math_number_property',
+                    kind: 'block',
+                    fields: {
+                        PROPERTY: 'EVEN',
+                    },
+                    inputs: {
+                        NUMBER_TO_CHECK: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 0,
+                                },
+                            },
+                        },
+                    },
+                },
+                {
+                    type: 'math_round',
+                    kind: 'block',
+                    fields: {
+                        OP: 'ROUND',
+                    },
+                    inputs: {
+                        NUM: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 3.1,
+                                },
+                            },
+                        },
+                    },
+                },
+                {
+                    type: 'math_modulo',
+                    kind: 'block',
+                    inputs: {
+                        DIVIDEND: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 64,
+                                },
+                            },
+                        },
+                        DIVISOR: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 10,
+                                },
+                            },
+                        },
+                    },
+                },
+                {
+                    type: 'math_random_int',
+                    kind: 'block',
+                    inputs: {
+                        FROM: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 1,
+                                },
+                            },
+                        },
+                        TO: {
+                            shadow: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 100,
+                                },
+                            },
+                        },
+                    },
+                },
+            ],
         },
         {
             kind: 'category',
